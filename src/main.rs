efce4febf73dc3b5c1c9a6ca38bebfc23bc3bbde
moritzbeste <bestemoritz3@gmail.com>
2026-04-22 23:31:48 +0200

    stopped using ratatui

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,11 @@ use std::{io, time::Duration};
 use crossterm::{
     event::{self, Event, KeyCode},
     execute,
-    terminal::{enable_raw_mode, disable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    terminal::{enable_raw_mode, disable_raw_mode, size, EnterAlternateScreen, LeaveAlternateScreen},
+    cursor::{Hide, Show},
+    style::{Color},
 };
 
-use ratatui::{
-    Terminal, backend::CrosstermBackend, style::Color
-};
 mod utility;
 mod world;
 mod hamilton;
@@ -18,15 +17,14 @@ use world::World;
 fn main() -> Result<(), io::Error> {
     enable_raw_mode()?;
     let mut stdout = io::stdout();
+    execute!(stdout, Hide)?;
     execute!(stdout, EnterAlternateScreen)?;
 
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    let size = terminal.size()?;
+    let (w, h) = size()?;
 
     // ============================================== Build World ==============================================
-    let width:  usize = if size.width  % 2 == 0 { size.width  as usize}  else { (size.width - 1) as usize};
-    let height: usize = if size.height % 2 == 0 { size.height  as usize} else { (size.height - 1) as usize};
+    let width:  usize = if w  % 2 == 0 { w  as usize}  else { (w - 1) as usize};
+    let height: usize = if h % 2 == 0 { h  as usize} else { (h - 1) as usize};
     assert!(width  >= 2, "width too small!");
     assert!(height >= 2, "height too small!");
     let n_parts: usize = 3;
@@ -37,18 +35,12 @@ fn main() -> Result<(), io::Error> {
     
     // =============================================== Game Loop ===============================================
     loop {
-        // draw frame
-        terminal.draw(|f| {
-            let size = f.area();
-            f.render_widget(&world, size);
-        })?;
-
         // simulation step
         world.simulation_step();
         //thread::sleep(Duration::from_millis(10));
 
         // handle input
-        if event::poll(Duration::from_millis(1))? {
+        if event::poll(Duration::from_millis(0))? {
             if let Event::Key(key) = event::read()? {
                 if key.code == KeyCode::Char('q') {
                     break;
@@ -59,8 +51,7 @@ fn main() -> Result<(), io::Error> {
     // =========================================================================================================
 
     disable_raw_mode()?;
-    execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
-    terminal.show_cursor()?;
-
+    execute!(stdout, LeaveAlternateScreen)?;
+    execute!(stdout, Show)?;
     Ok(())
 }
--- src/world.rs
+++ src/world.rs
@@ -1,48 +1,27 @@
 use crate::{snake::{BodySegment, Snake}, utility::{Direction, Uvec2}};
 use std::collections::{HashSet};
-
-use rand::{thread_rng, prelude::IteratorRandom};
-use ratatui::{
-    buffer::{Cell, Buffer}, layout::{Rect}, widgets::Widget, style::{Color, Style}
+use std::io::{stdout};
+use crossterm::{
+    execute,
+    cursor::MoveTo,
+    style::{Print, SetForegroundColor, SetBackgroundColor, ResetColor, Color},
 };
+use rand::{thread_rng, prelude::IteratorRandom};
 
 pub struct World {
     size: Uvec2,
-    frame: Vec<Cell>,
     color_snake: Color,
     color_food: Color,
     n_steps: u32,
     n_parts: u32,
     empty: HashSet<Uvec2>,
     snake: Snake,
-    food_coordinates: Uvec2,
-}
-
-impl Widget for &World {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let width = self.size.x;
-        let height = self.size.y;
-
-        for y in 0..height {
-            for x in 0..width {
-                let idx = y * width + x;
-                let cell: &Cell = &self.frame[idx];
-
-                if let Some(dst) = buf.cell_mut((area.x + x as u16, area.y + y as u16)) {
-                    *dst = cell.clone();
-                }
-            }
-        }
-    }
+    food_pos: Uvec2,
 }
 
 impl World {
     pub fn new(width: usize, height: usize, n_parts: usize, color_snake: Color, color_food: Color) -> Self {
         let size: Uvec2 = Uvec2 { x: width, y: height };
-
-        // ================================================ Build Frame ================================================
-        let mut frame : Vec<Cell> = vec![Cell::default(); size.x * size.y];
-        // =============================================================================================================
 
         // ================================================ Build Empty ================================================
         let mut empty = HashSet::new();
@@ -62,28 +41,25 @@ impl World {
         let snake: Snake = Snake::new(start, n_parts, size, facing);
         // add snake position to data structures
         empty.remove(&start);
-        let start_index: usize = start.y * size.x + start.x;
-        frame[start_index].set_char(Direction::get_connection(Direction::opposite(facing), facing));
-        frame[start_index].set_style(Style::default().fg(color_snake));
+        let c1: char = Direction::get_connection(Direction::opposite(facing), facing);
+        Self::draw_cell(start, c1, color_snake);
         // =============================================================================================================
 
         // ================================================= Set Food ==================================================
-        let food_coordinates: Uvec2 = Self::find_random_empty(&empty);
-        let food_index: usize = food_coordinates.y * size.x + food_coordinates.x;
-        frame[food_index].set_char(Self::get_food_symbol());
-        frame[food_index].set_style(Style::default().fg(color_food));
+        let food_pos: Uvec2 = Self::find_random_empty(&empty);
+        let c2: char = Self::get_food_symbol();
+        Self::draw_cell(food_pos, c2, color_food);
         // =============================================================================================================
 
         Self {
             size: size,
-            frame: frame,
             color_snake: color_snake, 
             color_food: color_food,
             n_steps: 0, 
             n_parts: n_parts as u32, 
             empty: empty,
             snake: snake, 
-            food_coordinates: food_coordinates 
+            food_pos: food_pos 
         }
     }
 
@@ -94,13 +70,13 @@ impl World {
     pub fn simulation_step(&mut self) {
         self.n_steps += 1;
         // =============================================== Update Snake ================================================
-        let (clear_option, new_pos) = self.snake.step(self.food_coordinates);
+        let (clear_option, new_pos) = self.snake.step(self.food_pos);
         // =============================================================================================================
 
         // ================================== Handle Collision & Update Empty, Frame ===================================
         if let Some(pos) = clear_option && self.n_steps >= self.n_parts {
             self.empty.insert(pos);
-            self.update_frame(pos, ' ', Color::Reset);
+            Self::draw_cell(pos, ' ', Color::Reset);
         }
         if !self.empty.contains(&new_pos) {
             panic!("Collision!");
@@ -108,25 +84,19 @@ impl World {
         self.empty.remove(&new_pos);
         let head: &BodySegment = self.snake.peek_head();
         let c1: char = Direction::get_connection(head.get_from(),head.get_to());
-        self.update_frame(new_pos, c1, self.color_snake);
+        Self::draw_cell(new_pos, c1, self.color_snake);
         if let Some(second) = self.snake.peek_second() {
             let c2: char = Direction::get_connection(second.get_from(),second.get_to());
-            self.update_frame(second.get_coordinates(), c2, self.color_snake);
+            Self::draw_cell(second.get_coordinates(), c2, self.color_snake);
         }
         // =============================================================================================================
 
         // ================================================ Handle Food ================================================
-        if new_pos == self.food_coordinates {
-            self.food_coordinates = Self::find_random_empty(&self.empty);
-            self.update_frame(self.food_coordinates, Self::get_food_symbol(), self.color_food);
+        if new_pos == self.food_pos {
+            self.food_pos = Self::find_random_empty(&self.empty);
+            Self::draw_cell(self.food_pos, Self::get_food_symbol(), self.color_food);
         }
         // =============================================================================================================
-    }
-
-    fn update_frame(&mut self, pos: Uvec2, c: char, color: Color) {
-        let index: usize = pos.y * self.size.x + pos.x;
-        self.frame[index].set_char(c);
-        self.frame[index].set_style(Style::default().fg(color));
     }
 
     fn find_random_empty(empty: &HashSet<Uvec2>) -> Uvec2 {
@@ -136,5 +106,17 @@ impl World {
 
     fn get_food_symbol() -> char {
         '\u{25CF}' // ●
+    }
+
+    fn draw_cell(pos: Uvec2, ch: char, fg: Color) {
+        let mut out = stdout();
+        execute!(
+            out,
+            MoveTo(pos.x as u16, pos.y as u16),
+            SetForegroundColor(fg),
+            SetBackgroundColor(Color::Reset),
+            Print(ch),
+            ResetColor
+        ).unwrap();
     }
 }
